0f6730807b2f920c37e5988ffe5b9e0670b6d7e2
manes <manes.hornung@gmail.com>
2019-02-06 12:14:47 +0100

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='simujob',
+      version='1.0',
+      description='A lightweight tool for simulations on a grid engine',
+      url='https://github.com/manesho/SimuJob',
+      author='Manes Hornung',
+      author_email='manes.hornung@gmail.com',
+      packages=['simujob'],
+      zip_safe=False)
